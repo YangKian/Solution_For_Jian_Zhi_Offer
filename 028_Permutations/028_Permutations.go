f22a9721a5f01616b8f947d0ab392ff3056f55fc
YangKian <45479280+YangKian@users.noreply.github.com>
2020-01-03 17:01:58 +0800

    add 25, 28, 29, 30, 31, 32, 34

---
+++ 028_Permutations/028_Permutations.go
@@ -0,0 +1,33 @@
+package main
+
+//Time：O(n*n!), Space:(n)
+func permute(nums []int) [][]int {
+	if len(nums) == 0 {
+		return [][]int{}
+	}
+
+	var result [][]int
+	permuteRec(nums, 0, &result)
+	return result
+}
+
+func permuteRec(nums []int, start int, res *[][]int) {
+	length := len(nums)
+	if start == length { //递归终止条件
+		temp := make([]int, length)
+		copy(temp, nums)
+		*res = append(*res, temp)
+		return
+	}
+
+	for i := start; i < length; i++ {
+		nums[i], nums[start] = nums[start], nums[i]
+		permuteRec(nums, start+1, res)
+		nums[i], nums[start] = nums[start], nums[i]
+	}
+}
+
+//func main() {
+//	nums := []int{1, 2, 3}
+//	permute(nums)
+//}
--- util/utils.go
+++ util/utils.go
@@ -11,3 +11,17 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
